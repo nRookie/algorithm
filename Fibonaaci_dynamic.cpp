6a41252ca91fa19c7459ac390f23240dd89d7786
kestrel <228611676@qq.com>
2020-01-01 01:20:30 -0500

    fix(dynamic): border condition error

--- Fibonaaci_dynamic.cpp
+++ Fibonaaci_dynamic.cpp
@@ -4,13 +4,14 @@ int Fib[1000000] = {0};
 
 void Fibonacci(int n)
 {
-  for (int i=0;i<n;i++)
+  for (int i=0;i<=n;i++)
   {
       if(i==0||i==1)
       Fib[i] = i;
       else
       {
         Fib[i] = Fib[i-1]+Fib[i-2];
+        printf("Fib%d:%d\n",i,Fib[i]);
       }
   }
 
@@ -21,7 +22,7 @@ int main()
   int n = 0;
 
   scanf("%d",&n);
-
+  Fibonacci(n);
   printf("F%d:%d",n,Fib[n]);
   return 0;
 }
